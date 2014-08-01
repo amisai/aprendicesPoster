36eff62ba3c4e911140c6fa9e18a642a627f7763
Abel Cuenca <abel.cuenca@gmail.com>
2014-08-01 20:23:58 +0200

    using value from configuration for number of elements in rss

--- lib/job/rss_generator.rb
+++ lib/job/rss_generator.rb
@@ -12,7 +12,7 @@ class RSSGenerator
       maker.channel.title = "Aprendices Feed"
       maker.channel.description = "RSS Feed with posts published in Aprendices community"
 
-      posts.take(20).each do |post|
+      posts.take(ENV['elements_in_rss'].to_i).each do |post|
         maker.items.new_item do |item|
           item.link = post.url
           item.title = post.text
