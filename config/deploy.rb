f5957d60e92ea837b033264576ba3f22d6e66c56
Abel Cuenca <abel.cuenca@gmail.com>
2014-08-12 18:25:07 +0200

    updating shared files and folders

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,10 +20,10 @@ set :deploy_to, "/usr/local/tools/bin/#{fetch(:application)}"
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{.env startLocalSinatra.sh  stopCurrentApp.sh}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
